6d4324a1888b6891b1f82961bbcc0638615fd281
Luka Muzinic <luka@muzinic.net>
2018-09-21 15:33:35 +0200

    Adds SimpleRuleBookTest

--- tests/RuleBook/SimpleRuleBookTest.php
+++ tests/RuleBook/SimpleRuleBookTest.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 namespace BallGame\Tests\RuleBook;
 
 use BallGame\RuleBook\SimpleRuleBook;
+use BallGame\Standings\TeamPosition;
+use BallGame\Team\Team;
 use PHPUnit\Framework\TestCase;
 
 class SimpleRuleBookTest extends TestCase
@@ -13,8 +15,50 @@ class SimpleRuleBookTest extends TestCase
      */
     protected $simpleRuleBook;
 
+    /**
+     * @var TeamPosition|\PHPUnit_Framework_MockObject_MockObject $teamAPosition
+     */
+    protected $teamAPosition;
+
+    /**
+     * @var TeamPosition|\PHPUnit_Framework_MockObject_MockObject $teamAPosition
+     */
+    protected $teamBPosition;
+
     public function setUp()
     {
+        $this->teamAPosition = $this->getMockBuilder(TeamPosition::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $this->teamBPosition = $this->getMockBuilder(TeamPosition::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
         $this->simpleRuleBook = new SimpleRuleBook();
+    }
+
+    public function testDecideReturnsLessThenZeroWhenFirstPositionIsGreaterThanSecond()
+    {
+        $this->teamAPosition->method('getPoints')->willReturn(10);
+        $this->teamBPosition->method('getPoints')->willReturn(1);
+
+        $this->assertSame(-1, $this->simpleRuleBook->decide($this->teamAPosition, $this->teamBPosition));
+    }
+
+    public function testDecideReturnsGreaterThanZeroWhenSecondPositionIsGreaterThanFirst()
+    {
+        $this->teamAPosition->method('getPoints')->willReturn(1);
+        $this->teamBPosition->method('getPoints')->willReturn(10);
+
+        $this->assertSame(1, $this->simpleRuleBook->decide($this->teamAPosition, $this->teamBPosition));
+    }
+
+    public function testDecideReturnsZeroWhenBothPositionsAreTheSame()
+    {
+        $this->teamAPosition->method('getPoints')->willReturn(10);
+        $this->teamBPosition->method('getPoints')->willReturn(10);
+
+        $this->assertSame(0, $this->simpleRuleBook->decide($this->teamAPosition, $this->teamBPosition));
     }
 }
