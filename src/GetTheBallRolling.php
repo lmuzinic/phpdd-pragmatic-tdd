17c8452f30eb67b504ebd656a24bf5f172733219
Luka Muzinic <luka@muzinic.net>
2018-09-21 11:14:02 +0200

    Adds BinaryGap and OddOccurrences

--- src/GetTheBallRolling.php
+++ src/GetTheBallRolling.php
@@ -23,4 +23,45 @@ class GetTheBallRolling
     {
         return $this->name;
     }
+
+    public function getBinaryGap(int $int)
+    {
+        $binary = decbin($int);
+
+        $counting = false;
+        $gap = 0;
+        for ($i = 0; $i < strlen($binary); $i += 1) {
+            if ($counting == true && $binary[$i] == 1) {
+                $counting = false;
+                continue;
+            }
+
+            if ($counting == false && $binary[$i] == 1) {
+                $counting = true;
+                continue;
+            }
+
+            if ($counting && $binary[$i] == 0) {
+                $gap += 1;
+            }
+        }
+
+        return $gap;
+    }
+
+    public function getOddOccurrences(array $input)
+    {
+        $countedInput = [];
+        foreach ($input as $item) {
+            if (!isset($countedInput[$item])) {
+                $countedInput[$item] = 1;
+            } else {
+                $countedInput[$item] += 1;
+            }
+        }
+
+        $flippedInput = array_flip($countedInput);
+
+        return $flippedInput[1];
+    }
 }
--- tests/GetTheBallRollingTest.php
+++ tests/GetTheBallRollingTest.php
@@ -8,10 +8,86 @@ use PHPUnit\Framework\TestCase;
 
 class GetTheBallRollingTest extends TestCase
 {
+    /**
+     * @var GetTheBallRolling
+     */
+    private $ball;
+
+    public function setUp()
+    {
+        $this->ball = GetTheBallRolling::create('Hello');
+    }
+
     public function testGetNameReturnsCreatedName()
     {
-        $ball = GetTheBallRolling::create('Hello');
+        $this->assertSame('Hello', $this->ball->getName());
+    }
 
-        $this->assertSame('Hello', $ball->getName());
+    public function testBinaryGapWhenThereAreNoGaps()
+    {
+        // 111
+        $binaryGap = $this->ball->getBinaryGap(7);
+
+        $this->assertSame(0, $binaryGap);
+    }
+
+    public function testBinaryGapWhenThereGapSizeTwo()
+    {
+        // 1001
+        $binaryGap = $this->ball->getBinaryGap(9);
+
+        $this->assertSame(2, $binaryGap);
+    }
+
+    public function testBinaryGapWhenThereAreTwoGaps()
+    {
+        // 10000100
+        $binaryGap = $this->ball->getBinaryGap(132);
+
+        $this->assertSame(4, $binaryGap);
+    }
+
+    public function testBinaryGapWhenThereAreTwoGapsAnTheLatterIsGreater()
+    {
+        // 11010001
+        $binaryGap = $this->ball->getBinaryGap(209);
+
+        $this->assertSame(3, $binaryGap);
+    }
+
+    public function testOddOccurrencesWhenThereIsOnlyOneElement()
+    {
+        $input = [42];
+
+        $oddOccurrences = $this->ball->getOddOccurrences($input);
+
+        $this->assertSame(42, $oddOccurrences);
+    }
+
+    public function testOddOccurrencesWhenThereAreThreeElements()
+    {
+        $input = [1, 42, 1];
+
+        $oddOccurrences = $this->ball->getOddOccurrences($input);
+
+        $this->assertSame(42, $oddOccurrences);
+    }
+
+    public function testOddOccurrencesWhenThereAreThreeElementsAndOddOneIsFirst()
+    {
+        $input = [42, 1, 1];
+
+        $oddOccurrences = $this->ball->getOddOccurrences($input);
+
+        $this->assertSame(42, $oddOccurrences);
+    }
+
+    public function testOddOccurrencesWhenThereAreThreeElementsAndOddOneIsLast()
+    {
+        $input = [42, 1, 1];
+
+        $oddOccurrences = $this->ball->getOddOccurrences($input);
+
+        $this->assertSame(42, $oddOccurrences);
     }
 }
